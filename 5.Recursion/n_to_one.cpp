a863830ace04ccf4e272f73fc0673cdd9f2e6c53
dheerajsharma0328 <dheerajsharma0328@gmail.com>
2026-08-03 01:36:15 +0530

    n to one

---
+++ 5.Recursion/n_to_one.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void n_to_one(int number);
+
+int main()
+{
+    int number;
+    cout<<"Enter Number: ";
+    cin>>number;
+
+    n_to_one(number);
+
+    return 0;
+
+}
+
+void n_to_one(int number)
+{
+    cout<<"Count: "<<number<<endl;;
+
+    if(number == 1)
+    {
+        return;
+    }
+
+    number--;
+    n_to_one(number);
+}
